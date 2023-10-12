9f75e0de756f4271d2d2cde4c2466936bf873e5e
unknown <dylanosborn123@hotmail.com>
2023-10-11 21:25:46 -0500

    Added save Score and Initials function

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,38 @@
 const questions = [
     {
-        question: "What does HTML stand for?", 
+        question: "Which of the following is not a valid data type in JavaScript?", 
         answers: [
-            { text: "A) Hyper Text Markup Language", correct: true},
-            { text: "B) High Technology Modern Language", correct: false},
-            { text: "C) Hyperlinks and Text Markup Language", correct: false},
-            { text: "D) Home Tool Markup Language", correct: false},
+            { text: " A) Number ", correct: false},
+            { text: " B) String ", correct: false},
+            { text: " C) Character ", correct: true},
+            { text: " D) Boolean ", correct: false},
         ]
     },
     {
-        question: "What does CSS stand for?", 
+        question: "What does the `===` operator in JavaScript do?", 
         answers: [
-            { text: "A) Cascading Style Script", correct: false},
-            { text: "B) Computer Style Sheet", correct: false},
-            { text: "C) Creative Style System", correct: false},
-            { text: "D) Cascading Style Sheets", correct: true},
+            { text: " A) Compares two values for equality without type conversion. ", correct: true},
+            { text: " B) Assigns a value to a variable. ", correct: false},
+            { text: " C) Compares two values for equality with type conversion. ", correct: false},
+            { text: " D) Checks if a variable is defined. ", correct: false},
         ]
     },
     {
         question: "Which keyword is used to declare a variable in JavaScript?", 
         answers: [
-            { text: "A) var", correct: true},
-            { text: "B) let", correct: false},
-            { text: "C) variable", correct: false},
-            { text: "D) v", correct: false},
+            { text: " A) v ", correct: false},
+            { text: " B) let ", correct: false},
+            { text: " C) variable ", correct: false},
+            { text: " D) var ", correct: true},
         ]
     },
     {
-        question: "Which HTML tag is used for creating a hyperlink?", 
+        question: "Which of the following is the correct way to write a single-line comment in JavaScript?", 
         answers: [
-            { text: "A) <link>", correct: false},
-            { text: "B) <href>", correct: false},
-            { text: "C) <a>", correct: true},
-            { text: "D) <http>", correct: false},
+            { text: " A) `/* This is a comment */` ", correct: false},
+            { text: " B) `// This is a comment` ", correct: true},
+            { text: " C) `# This is a comment` ", correct: false},
+            { text: " D) `<!-- This is a comment -->` ", correct: false},
         ]
     }
 ];
@@ -45,51 +45,55 @@ const startQuizBtn = document.getElementById("startQuizBtn");
 let secondsLeft = 10;
 let currentQuestionIndex = 0;
 let score = 0;
+let timerInterval;
+let saveBtn = $("#saveBtn");
+const initialsForm = $("#initials-form")
+
+
 
 function setTime() {
     // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = secondsLeft;
-  
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-        showScore();
-      }
-  
-    }, 1000);
-}
+    timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+        
+        if(secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to create and append image
+            showScore();
+        }
+        
+    }, 1500);
+};
 
 function sendMessage() {
     timeEl.textContent = "Time's Up";
-    showScore();
-}
+};
 
 function startQuizFunc() {
     var quizContainer = document.querySelector(".quizContainer");
-
+    
     startQuiz();
-}
+};
 
 function startQuiz() {
     var quizContainer = document.querySelector(".quizContainer");
     currentQuestionIndex = 0;
     score = 0;
-    quizContainer.style.display = 'block'
-    startQuizBtn.style.display = 'none'
+    quizContainer.style.display = 'block';
+    startQuizBtn.style.display = 'none';
     nextBtn.textContent = "Next";
     showQuestion();
     setTime();
-}
+};
 
 function showQuestion() {
     resetState();
+    initialsForm.hide();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionEl.textContent = questionNo + ". " + currentQuestion.question;
-
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.textContent = answer.text;
@@ -98,32 +102,30 @@ function showQuestion() {
         if(answer.correct){
             button.dataset.correct = answer.correct;
         }
-        button.addEventListener("click", selectAnswer)
+        button.addEventListener("click", selectAnswer);
     });
-}
+};
 
 function resetState() {
     nextBtn.style.display = "none";
     while(answerBtns.firstChild) {
         answerBtns.removeChild(answerBtns.firstChild);
     }
-}
+};
 
 function selectAnswer(event) {
     const selectedBtn = event.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct")
-        score ++;
-        secondsLeft += 2;
+        score += 5;
+        secondsLeft += 3;
     } else {
         selectedBtn.classList.add("incorrect")
-        secondsLeft -= 2;
-
+        secondsLeft -= 3;
+        
         if(secondsLeft === 0) {
-            // Stops execution of action at set interval
             clearInterval(timerInterval);
-            // Calls function to create and append image
             showScore();
         }
     }
@@ -134,22 +136,21 @@ function selectAnswer(event) {
         button.disabled = true;
     });
     nextBtn.style.display = "block"
-}
+};
 
 function showScore(){
     resetState();
-    questionEl.textContent = `You scored ${score} out of ${questions.length}!`;
-    nextBtn.textContent = 'Play again';
+    clearInterval(timerInterval);
+    sendMessage();
+    questionEl.textContent = `You scored ${score} out of 20!`;
     nextBtn.style.display = "block";
-
+    initialsForm.show();
+    nextBtn.textContent = 'Play again';
     nextBtn.addEventListener("click", function() {
-        window.location.reload(); // Reloads the page
+        window.location.reload();
     });
-}
-
-function quizEnd() {
     
-}
+};
 
 function handleNextBtn(){
     currentQuestionIndex++;
@@ -158,8 +159,7 @@ function handleNextBtn(){
     } else {
         showScore();
     }
-}
-
+};
 
 nextBtn.addEventListener("click", ()=>{
     if(currentQuestionIndex < questions.length){
@@ -167,7 +167,13 @@ nextBtn.addEventListener("click", ()=>{
     }
 });
 
-
 startQuizBtn.addEventListener("click", function(event){
     startQuiz();
+});
+
+saveBtn.on("click", (event)=>{
+    const userInitials = $("#initials").val();
+    event.preventDefault();
+    localStorage.setItem("Initials", userInitials);
+    localStorage.setItem("Score", score);
 });
